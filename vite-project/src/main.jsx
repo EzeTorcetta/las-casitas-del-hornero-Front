eebea8a95e4e4081b0d28b293e29546138844669
uliseschambi <uliseschambi@gmail.com>
2023-05-08 18:51:35 -0300

    search bar bootstrap

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import store from "./redux/Store/Store.js";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import store from './redux/Store/Store.js';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
