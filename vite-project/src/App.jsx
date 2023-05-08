47bcdd8b4bd553978c7bc795afd1a9b03872d754
Ignacio Martin <119757609+maximustxt@users.noreply.github.com>
2023-05-08 14:23:22 -0300

    REALICE CAMBIOS EN APP/LOADING/REDUX

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Home, Favoritos, Detail, Nav, Loading } from "./components/Index";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <Nav />
+        <Loading />
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Favoritos" element={<Favoritos />} />
+          <Route path="/Detail/:id" element={<Detail />} />
+          <Route path="/" />
+        </Routes>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,7 +39,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ vite-project/src/components/Index.js
@@ -0,0 +1,8 @@
+// ACA VAN LAS IMPORTACIONES DE LOS COMPONENTES PARA TENER MAS ORDENADO..
+import Home from "./Home/Home";
+import Favoritos from "./Favorites/Favorites";
+import Detail from "./Detail/Detail";
+import Nav from "./Nav/Nav";
+import Loading from "./Loading/Loading";
+
+export { Home, Favoritos, Detail, Nav, Loading };
