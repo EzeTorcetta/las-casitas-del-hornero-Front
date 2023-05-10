20dba370075e32a3c8f93ccd17003b1e14950e1b
Lypher <acosta.enzo@hotmail.com>
2023-05-10 14:54:26 -0300

    logo

--- vite-project/src/components/Nav/Nav.jsx
+++ vite-project/src/components/Nav/Nav.jsx
@@ -1,16 +1,23 @@
 // import { Navbar, Container, Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
-import style from './Nav.module.css';
+import { NavLink } from "react-router-dom";
+import style from "./Nav.module.css";
 
 const NavBar = () => {
   return (
     <nav className={style.nav}>
-      <div className={style.logo}>LOGO</div>
+      <div className={style.logo}>
+        <img
+          className={style.img}
+          src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/utbvsuv2bhb7gbubbaqk"
+          alt=""
+        />
+        <p>LaCasitaDelHornero </p>
+      </div>
       <div>
-        <NavLink to={'/Home'} className={style.link}>
+        <NavLink to={"/Home"} className={style.link}>
           Home
         </NavLink>
-        <NavLink to={'/Favoritos'} className={style.link}>
+        <NavLink to={"/Favoritos"} className={style.link}>
           Favoritos
         </NavLink>
       </div>
