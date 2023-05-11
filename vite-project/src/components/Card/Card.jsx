a5931d7c4d14e3472e0faa6ee665c30e4f9db1a1
uliseschambi <uliseschambi@gmail.com>
2023-05-11 12:16:23 -0300

    Card

--- vite-project/src/components/Card/Card.jsx
+++ vite-project/src/components/Card/Card.jsx
@@ -6,20 +6,22 @@ import style from "./Card.module.css";
 
 function Cards() {
   return (
-    <Row xs={1} md={2} className="g-2">
-      {hotels.map((hotel) => (
-        <Col key={hotel.id}>
-          {/* <Card className={`bg-dark text-white ${style.card}`}>
-            <Card.Img src={hotel.image} alt="Card image" className={style.img} />
-            <Card.ImgOverlay>
-              <Card.Title>{hotel.nombre}</Card.Title>
-              <Card.Text>{hotel.direccion}</Card.Text>
-              <Card.Text>{hotel.provincia}</Card.Text>
-            </Card.ImgOverlay>
-          </Card> */}
-        </Col>
-      ))}
-    </Row>
+    <div className={style.container}>
+      <Row xs={1} md={3} className="g-2">
+        {hotels.map((hotel) => (
+          <Col key={hotel.id}>
+            <Card className="bg-white text-white">
+              <Card.Img src={hotel.image} alt="Card image" />
+              <Card.ImgOverlay>
+                <Card.Title>{hotel.nombre}</Card.Title>
+                <Card.Text>{hotel.direccion}</Card.Text>
+                <Card.Text>{hotel.provincia}</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
   );
 }
 
