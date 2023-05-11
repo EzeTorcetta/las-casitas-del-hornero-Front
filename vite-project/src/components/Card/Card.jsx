828175cc426cbb22cc5c66e27f7451ac0d72d6cf
uliseschambi <uliseschambi@gmail.com>
2023-05-11 10:15:25 -0300

    Card Home data

--- vite-project/src/components/Card/Card.jsx
+++ vite-project/src/components/Card/Card.jsx
@@ -1,8 +1,26 @@
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import hotels from "../../data";
+import style from "./Card.module.css";
 
 function Cards() {
-  return <div>Cards</div>;
+  return (
+    <Row xs={1} md={2} className="g-4">
+      {hotels.map((hotel) => (
+        <Col key={hotel.id}>
+          <Card className="bg-dark text-white">
+            <Card.Img src={hotel.image} alt="Card image" className={style.img} />
+            <Card.ImgOverlay>
+              <Card.Title>{hotel.nombre}</Card.Title>
+              <Card.Text>{hotel.direccion}</Card.Text>
+              <Card.Text>{hotel.provincia}</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 }
 
 export default Cards;
--- vite-project/src/components/Home/Home.jsx
+++ vite-project/src/components/Home/Home.jsx
@@ -13,61 +13,38 @@ const Home = () => {
       <div className={style.container}>
         <Carousel className={style.carousel}>
           <Carousel.Item className={style.item}>
-            <img
-              // className="d-block w-100"
-              className={style.img}
-              src="https://www.murhotels.com/cache/40/b3/40b3566310d686be665d9775f59ca9cd.jpg"
-              alt="First slide"
-            />
+            <img className={style.img} src="https://www.murhotels.com/cache/40/b3/40b3566310d686be665d9775f59ca9cd.jpg" alt="First slide" />
             <Carousel.Caption>
               <h3>MUR Aparthotel Buenos Aires</h3>
-              <p>
-                Apartamentos en Playa del Inglés en San Bartolomé en Gran
-                Canaria.
-              </p>
+              <p>Apartamentos en Playa del Inglés en San Bartolomé en Gran Canaria.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item className={style.item}>
-            <img
-              // className="d-block w-100"
-              className={style.img}
-              src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/ce/0c/66/frente-nocturno.jpg?w=700&h=-1&s=1"
-              alt="Second slide"
-            />
+            <img className={style.img} src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/ce/0c/66/frente-nocturno.jpg?w=700&h=-1&s=1" alt="Second slide" />
             <Carousel.Caption>
               <h3>Hotel Patagonia</h3>
-              <p>
-                Toda la comodidad y unas vistas paradisíacas de la Patagonia
-                Argentina.
-              </p>
+              <p>Toda la comodidad y unas vistas paradisíacas de la Patagonia Argentina.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item className={style.item}>
             <img
-              // className="d-block w-100"
               className={style.img}
               src="https://assets.hyatt.com/content/dam/hyatt/hyattdam/images/2017/08/03/1943/Park-Hyatt-Mendoza-P041-Facade-Lateral-View.jpg/Park-Hyatt-Mendoza-P041-Facade-Lateral-View.16x9.jpg"
               alt="Third slide"
             />
             <Carousel.Caption>
               <h3>Park Hyatt Mendoza</h3>
-              <p>
-                Magnífica fachada Neoclásica de la academia francesa del siglo
-                XX.
-              </p>
+              <p>Magnífica fachada Neoclásica de la academia francesa del siglo XX.</p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
         <Search />
         <Clima />
 
-        <section className={`${style.section} ${style.one}`}>
-          <Card />
-        </section>
-        <section className={`${style.section} ${style.two}`}>
-          <h1 className={style.h1}>Optional</h1>
-        </section>
+        <section className={`${style.section} ${style.one}`}>{/* <Card /> */}</section>
+        <section className={`${style.section} ${style.two}`}></section>
       </div>
+
       <Footer />
     </>
   );
