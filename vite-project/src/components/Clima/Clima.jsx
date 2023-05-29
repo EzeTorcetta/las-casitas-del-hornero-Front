21d0c23fe695886977e6850d17d2d5df7d9425e9
Lypher <acosta.enzo@hotmail.com>
2023-05-29 09:32:51 -0300

    clima actualizado

--- vite-project/src/components/Clima/Clima.jsx
+++ vite-project/src/components/Clima/Clima.jsx
@@ -3,7 +3,7 @@ import style from "./Clima.module.css";
 import { useSelector } from "react-redux";
 
 const VITE_API_KEY = "bf29d6808d85484fb42200108231005";
-const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${VITE_API_KEY}&q=`;
+const API_WEATHER = `https://api.weatherapi.com/v1/forecast.json?key=${VITE_API_KEY}&q=`;
 
 export default function Clima() {
   const [city, setCity] = useState("");
@@ -16,10 +16,7 @@ export default function Clima() {
   const [weather, setWeather] = useState({
     city: "",
     country: "",
-    temp: "",
-    condition: "",
-    icon: "",
-    conditionText: "",
+    forecasts: [],
   });
 
   const idioma = useSelector((state) => state.idioma);
@@ -48,17 +45,14 @@ export default function Clima() {
 
     try {
       if (!city.trim()) throw { message: "El campo ciudad es obligatorio" };
-      const response = await fetch(`${API_WEATHER}${city}`);
+      const response = await fetch(`${API_WEATHER}${city}&days=7`); // Añadido el parámetro 'days' para obtener 7 días de pronóstico
       const data = await response.json();
       if (data.error) throw { message: data.error.message };
 
       setWeather({
         city: data.location.name,
         country: data.location.country,
-        temp: data.current.temp_c,
-        condition: data.current.condition.code,
-        icon: data.current.condition.icon,
-        conditionText: data.current.condition.text,
+        forecasts: data.forecast.forecastday, // Almacenar los pronósticos de varios días en 'forecasts'
       });
     } catch (error) {
       setError({
@@ -69,8 +63,9 @@ export default function Clima() {
       setLoading(false);
     }
   };
+
   return (
-    <div>
+    <div className={style.container}>
       <h2 className={style.h2}>{translations[idioma].Clima}</h2>
       <form className={style.form} autoComplete="off" onSubmit={onSubmit}>
         <input
@@ -88,15 +83,23 @@ export default function Clima() {
       </form>
 
       {weather.city && (
-        <div className={style.result}>
+        <div>
           <h4>
-            {weather.city},{weather.country}
+            {weather.city}, {weather.country}
           </h4>
-          <div>
-            <img src={weather.icon} alt={weather.conditionText} />
-          </div>
-          <h3>{weather.temp}ºC</h3>
-          <h4>{weather.condition}</h4>
+          {weather.forecasts.map((forecast) => (
+            <div key={forecast.date}>
+              <br />
+              <h5>{forecast.date}:</h5>
+              <div>
+                <img
+                  src={forecast.day.condition.icon}
+                  alt={forecast.day.condition.text}
+                />
+              </div>
+              <h3>{forecast.day.avgtemp_c}ºC</h3>
+            </div>
+          ))}
         </div>
       )}
 
